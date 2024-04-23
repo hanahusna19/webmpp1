96de551008bd5640032dbd37491c1ecd8ff384cf
github_username <email_address>
2024-04-23 11:00:30 +0700

    Detail Pengajuan

---
+++ application/views/detailpengajuan.php
@@ -0,0 +1,94 @@
+ <!-- Main content -->
+ <div class="invoice p-3 mb-3">
+
+              <div class="row">
+                <div class="col-12 table-responsive">
+                <table align="center" border="0" cellpadding="1" style="width: 700px;"><tbody>
+                        <tr>     <td colspan="3"><div align="center">
+                        <span style="font-family: Times New Roman; font-size: x-large;"><b>SURAT PENGAJUAN PEGAWAI IPB UNIVERSITY
+
+                        <hr />
+                        </div>
+                        </td>   </tr>
+                        <tr>     <td colspan="2"><table border="0" cellpadding="1" style="width: 400px;"><tbody>
+                        <tr>         <td width="93"><span style="font-size: x-small;">No</span></td>         <td width="8"><span style="font-size: x-small;">:</span></td>         <td width="200"><span style="font-size: x-small;">005/ ipb-univ/ yps/ IV/ 20xx</span></td>       </tr>
+                        <tr>         <td><span style="font-size: x-small;">Lampiran</span></td>         <td><span style="font-size: x-small;">:</span></td>         <td><span style="font-size: x-small;">-</span></td>       </tr>
+                        <tr>         <td><span style="font-size: x-small;">Perihal</span></td>         <td><span style="font-size: x-small;">:</span></td>         <td><span style="font-size: x-small;">Surat Pengajuan Pegawai</span></td>       </tr>
+                        <tr>         <td width="93"><span style="font-size: x-small;">Kepada yth </span></td>
+                        </tbody></table>
+                        </td>     <td valign="top"><div align="right">
+                        <span style="font-size: x-small;" id="tanggalwaktu">Tanggal/Waktu: </span></div>
+                        </td>   </tr>
+                        <tr>     <td colspan="2"><table border="0" cellpadding="1" style="width: 400px;"><tbody>
+                        <tr>         <td><span style="font-size: x-small;">Direktorat Sumber Daya Manusia IPB University</span></td>                         
+                        </tbody></table>
+
+                  
+                        <tr>     <td width="302"></td>     <td width="343"></td>     <td width="339"></td>   </tr>
+                        <tr>     <td><table border="0" style="width: 239px;"><tbody>
+                       </td>       </tr>
+                        <tr>         <td><span style="font-size: x-small;">di</span></td>         <td></td>         <td>
+                        </td>       </tr>
+                        <tr>         <td><span style="font-size: x-small;">tempat</span></td>         <td></td>         <td>
+                        </td>       </tr>
+                        </tbody></table>
+                        </td>     <td></td>     <td></td>   </tr>
+                        <tr>     <td></td>     <td></td>     <td></td>   </tr>
+                        <tr>     <td colspan="3" height="270" valign="top"><div align="justify">
+                        <pre><span style="font-size: x-small;">Dengan hormat,
+                        Bersama surat ini, dengan penuh rasa hormat dan kesadaran akan kebutuhan tim kami, kami ingin mengajukan permohonan untuk penambahan 
+                        tenaga kerja pada departemen kami.</span></pre>
+                        <table border="0" style="width: 352px;"><tbody>
+                        <tr>           <td width="80"><span style="font-size: x-small;">hari/tanggal</span></td>           <td width="10"><span style="font-size: x-small;">:</span></td>           <td width="248"><span style="font-size: x-small;" id="tanggalwaktu">Rabu, 11 mei 2011</span></td>         </tr>
+                        <tr>           <td><span style="font-size: x-small;">periode</span></td>           <td><span style="font-size: x-small;">:</span></td>           <td><span style="font-size: x-small;">08.00 - selesai</span></td>         </tr>
+                        <tr>           <td><span style="font-size: x-small;">departemen</span></td>           <td><span style="font-size: x-small;">:</span></td>           <td><span style="font-size: x-small;">---------</span></td>         </tr>
+                        </tbody></table>
+                        <div align="justify">
+                        <span style="font-size: x-small;">
+
+                        Demikian surat ini kami sampaikan, kami harap ibu/bapa dapat menghadiri menerima surat ini. sekian dan terima kasih.</span> </div>
+                        </div>
+                        <div align="center">
+                        <span style="font-size: x-small;">Mengetahui</span></div>
+                        </td>   </tr>
+                        <tr>     <td><div align="center">
+                        <span style="font-size: x-small;">Sekertaris,</span></div>
+                        <div align="center">
+
+                        </div>
+                        <div align="center">
+                        <span style="font-size: x-small;">------------ </span></div>
+                        </td>     <td></td>     <td valign="top"><div align="center">
+                        <span style="font-size: x-small;">Direktorat Sumber Daya Manusia, </span></div>
+                        <div align="center">
+
+                        </div>
+                        <div align="center">
+                        <span style="font-size: x-small;">---------------</span></div>
+                        </td>   </tr>
+                </div>
+                        
+            </tbody>
+                </table>
+
+            <script>
+                        var tw = new Date();
+                        if (tw.getTimezoneOffset() == 0) (a=tw.getTime() + ( 7 *60*60*1000))
+                        else (a=tw.getTime());
+                        tw.setTime(a);
+                        var tahun= tw.getFullYear ();
+                        var hari= tw.getDay ();
+                        var bulan= tw.getMonth ();
+                        var tanggal= tw.getDate ();
+                        var hariarray=new Array("Minggu,","Senin,","Selasa,","Rabu,","Kamis,","Jum'at,","Sabtu,");
+                        var bulanarray=new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","Nopember","Desember");
+                        document.getElementById("tanggalwaktu").innerHTML = hariarray[hari]+" "+tanggal+" "+bulanarray[bulan]+" "+tahun;
+                        </script>
+            <div class="row no-print">
+                <div class="col-12">
+                  <a href="<?= base_url('printpengajuan') ?>" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
+                </div>
+            </div>
+                </div>
+              </div>
+ </div>
