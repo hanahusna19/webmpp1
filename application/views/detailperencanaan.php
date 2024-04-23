3c3becb53088d97961b4d1787b16a1b316789156
github_username <email_address>
2024-04-23 11:00:59 +0700

    Detail Perencanaan

---
+++ application/views/detailperencanaan.php
@@ -0,0 +1,173 @@
+
+
+  <main id="main" class="main">
+ <section class="section profile">
+      <div class="row">
+        <div class="col-xl-12">
+          <div class="card">
+            <div class="card-body pt-3">
+              <div class="tab-content pt-2">
+
+                <div class="tab-pane fade show active profile-overview" id="profile-overview">
+                
+                  <style>
+    /* Custom CSS styles */
+    .profile-img {
+      max-width: 200px;
+      border-radius: 50%;
+    }
+  </style>
+</head>
+<body>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-4 text-center">
+        <img src="https://via.placeholder.com/200" alt="Profile Picture" class="profile-img img-fluid">
+      </div>
+      <div class="col-md-8">
+      <div class="row">
+                    <div class="col-lg-3 col-md-4 label">Nama Pengajuan</div>
+                    <div class="col-lg-9 col-md-8">M Fatih Syaban</div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-lg-3 col-md-4 label">Posisi Jabatan</div>
+                    <div class="col-lg-9 col-md-8">Staff Enginer</div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-lg-3 col-md-4 label">Tanggal </div>
+                    <div class="col-lg-9 col-md-8">27 November 2023</div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-lg-3 col-md-4 label">Periode </div>
+                    <div class="col-lg-9 col-md-8">2023</div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-lg-3 col-md-4 label">Notes </div>
+                    <div class="col-lg-9 col-md-8">-</div>
+                  </div>
+    </div>
+  </div>
+  <table class="table table-bordered datatable">
+                    <thead>
+                    <tr>
+        <th rowspan="2" scope="col">Posisi Jabatan</th>
+        <th colspan="2" scope="colgroup">Jan</th>
+        <th colspan="2" scope="colgroup">Feb</th>
+        <th colspan="2" scope="colgroup">Mar</th>
+        <th colspan="2" scope="colgroup">Apr</th>
+        <th colspan="2" scope="colgroup">Mei</th>
+        <th colspan="2" scope="colgroup">Juni</th>
+        <th colspan="2" scope="colgroup">Juli</th>
+        <th colspan="2" scope="colgroup">Ags</th>
+        <th colspan="2" scope="colgroup">Sep</th>
+        <th colspan="2" scope="colgroup">Okt</th>
+        <th colspan="2" scope="colgroup">Nov</th>
+        <th colspan="2" scope="colgroup">Des</th>
+          </tr>  
+        </thead>
+          <tbody>
+            <tr>
+        <td rowspan="" scope="colgroup"> <select class="form-control">
+            <option value="pilih">Pilih</option>
+            <option value="manager">Manager</option>
+            <option value="developer">Developer</option>
+            <option value="designer">Designer</option>
+          </select></td>
+        <td>1</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>10</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>10</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>1</td>
+        <td>10</td>
+        <td>10</td>
+      </tr>
+          </tbody>
+                    </table>
+      </table>
+
+</div>
+</body>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
